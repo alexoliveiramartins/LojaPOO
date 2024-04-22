86dc6b47d6ce264e08aaccdd1b7168db17f1fd86
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-04-22 15:27:30 -0300

    Create main.java

---
+++ aula5/main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    Produto produto1 = new Produto(1, "Camisa do Flamengo", "Rubro-Negra", 10.99, 50);
+    Produto produto2 = new Produto(2, "Shorts", "Shorts Branco", 10.99, 50);
+  }
+}
