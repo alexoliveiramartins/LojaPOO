d923561bc5b77ac10f2f18bb290219269a09962e
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-04-27 18:42:25 -0300

    Update main.java
    Adicionada validação de cpf/cnpj e criação de pessoas na main.

--- aula5/main.java
+++ aula5/main.java
@@ -1,8 +1,18 @@
+import javax.swing.JOptionPane;
+import java.lang.*;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-
-    Produto produto1 = new Produto(1, "Camisa do Flamengo", "Rubro-Negra", 10.99, 50);
-    Produto produto2 = new Produto(2, "Shorts", "Shorts Branco", 10.99, 50);
+	  
+	  Pessoa pessoa1 = new PessoaFisica(1, "Alex", "12345678912");
+	  boolean valido = ((PessoaFisica) pessoa1).validarCpf();
+	  if(valido == true) System.out.println("CPF VALIDO!");
+	  else JOptionPane.showMessageDialog(null, "CPF INVALIDO!", "alerta", JOptionPane.ERROR_MESSAGE);
+	  
+	  Pessoa empresa1 = new PessoaJuridica(2, "Simas Turbo", "71489715000106");
+	  boolean cnpjValido = ((PessoaJuridica) empresa1).validarCnpj();
+	  if(cnpjValido == true) System.out.println("CNPJ VALIDO!");
+	  else JOptionPane.showMessageDialog(null, "CNPJ INVALIDO!", "alerta", JOptionPane.ERROR_MESSAGE);
+	
   }
 }
