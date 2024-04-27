3c3da57e63f019a60056f1241eabe8c0bdfbac58
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-04-27 20:22:01 -0300

    Update Pedido.java

--- aula5/Pedido.java
+++ aula5/Pedido.java
@@ -1,35 +1,55 @@
+import java.util.*;
+
 public class Pedido {
-  private int idPedido;
-  private String status;
-  private int idCliente;
+	private int idPedido;
+	private String status;
+	private int idCliente;
+	private static ArrayList<Item> pedido = new ArrayList<>();
 
-  public Pedido(int idPedido, String status, int idCliente) {
-    this.idPedido = idPedido;
-    this.status = status;
-    this.idCliente = idCliente;
-  }
+	public Pedido(int idPedido, String status, int idCliente) {
+		this.idPedido = idPedido;
+		this.status = status;
+		this.idCliente = idCliente;
+	}
 
-  public int getIdPedido() {
-    return idPedido;
-  }
+	public int getIdPedido() {
+		return idPedido;
+	}
 
-  public void setIdPedido(int idPedido) {
-    this.idPedido = idPedido;
-  }
+	public void setIdPedido(int idPedido) {
+		this.idPedido = idPedido;
+	}
 
-  public String getStatus() {
-    return status;
-  }
+	public String getStatus() {
+		return status;
+	}
 
-  public void setStatus(String status) {
-    this.status = status;
-  }
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-  public int getIdCliente() {
-    return idCliente;
-  }
+	public int getIdCliente() {
+		return idCliente;
+	}
 
-  public void setIdCliente(int idCliente) {
-    this.idCliente = idCliente;
-  }
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public static ArrayList<Item> getPedido() {
+		return pedido;
+	}
+
+	public static void adicionarItem(Item item) {
+		pedido.add(item);
+	}
+
+	public double calcularTotal() {
+		int tam = pedido.size();
+		int soma = 0;
+		for(int c = 0; c < tam; c++) {
+			soma += pedido.get(c).calcularSubTotal();
+		}
+		return soma;
+	}
 }
