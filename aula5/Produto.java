0653833e004a6a8e6bdd9afaf80ebcc5103ec8ab
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-04-27 20:21:46 -0300

    Update Produto.java

--- aula5/Produto.java
+++ aula5/Produto.java
@@ -1,15 +1,55 @@
 public class Produto {
   int idProduto;
-  int idPedido;
-  int idItemPedido;
-  float preco;
+  String nome;
+  String descricao;
+  double preco;
   int estoque;
 
-  public Produto(int idProduto, int idPedido, int idItemPedido, float preco, int estoque) {
+  public Produto(int idProduto, String nome, String descricao, double preco, int estoque) {
     this.idProduto = idProduto;
-    this.idPedido = idPedido;
-    this.idItemPedido = idItemPedido;
+    this.nome = nome;
+    this.descricao = descricao;
     this.preco = preco;
     this.estoque = estoque;
-    }
+  }
+
+  public int getIdProduto() {
+    return idProduto;
+  }
+
+  public void setIdProduto(int idProduto) {
+    this.idProduto = idProduto;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public double getPreco() {
+    return preco;
+  }
+
+  public void setPreco(double preco) {
+    this.preco = preco;
+  }
+
+  public int getEstoque() {
+    return estoque;
+  }
+
+  public void setEstoque(int estoque) {
+    this.estoque = estoque;
+  }
 }
