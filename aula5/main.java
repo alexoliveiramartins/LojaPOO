d9867d7595b6927135c7e58b9374ed958f6c2d9e
alexoliveiramartins <155112179+alexoliveiramartins@users.noreply.github.com>
2024-04-28 13:15:02 -0300

    Update main.java
    > Termina a execução em caso de CPF/CNPJ inválido;
    > Implementada a função de atualizar estoque, atualizar status do pedido, calcular total do pedido;
    > Criadas caixas de diálogo para visualização/confirmação de pedido, pedido finalizado/cancelado;

--- aula5/main.java
+++ aula5/main.java
@@ -3,34 +3,64 @@ import java.lang.*;
 
 public class Main {
   public static void main(String[] args) {
+		  
+	  // criacao de pessoas e validacao de cnpj/cpf
 	  
-	  Pessoa pessoa1 = new PessoaFisica(1, "Alex", "12345678912");
+	  Pessoa pessoa1 = new PessoaFisica(1, "Alex", "07203321177");
 	  boolean valido = ((PessoaFisica) pessoa1).validarCpf();
 	  if(valido == true) System.out.println("CPF VALIDO!");
-	  else JOptionPane.showMessageDialog(null, "CPF INVALIDO!", "alerta", JOptionPane.ERROR_MESSAGE);
+	  else{
+		  JOptionPane.showMessageDialog(null, "CPF INVALIDO!", "alerta", JOptionPane.ERROR_MESSAGE);
+		  return;
+	  }
 	  
 	  Pessoa empresa1 = new PessoaJuridica(2, "Simas Turbo", "71489715000106");
 	  boolean cnpjValido = ((PessoaJuridica) empresa1).validarCnpj();
 	  if(cnpjValido == true) System.out.println("CNPJ VALIDO!");
-	  else JOptionPane.showMessageDialog(null, "CNPJ INVALIDO!", "alerta", JOptionPane.ERROR_MESSAGE);
+	  else{
+		  JOptionPane.showMessageDialog(null, "CNPJ INVALIDO!", "alerta", JOptionPane.ERROR_MESSAGE);
+		  return;
+	  }
+	  
+	  // criacao do pedido/ produtos/ itens
 	  
 	  Pedido pedido1 = new Pedido(1, "Pendente", pessoa1.getId());
 	  
 	  Produto produto1 = new Produto(1, "Refrigerante", "Pitchula", 19.99, 20);
 	  Produto produto2 = new Produto(2, "Bolacha", "Trakinas", 5.99, 639);
+	  
 	  Item item1 = new Item(produto1, 3);
 	  Item item2 = new Item(produto2, 5);
 	  
 	  Pedido.adicionarItem(item1);
 	  Pedido.adicionarItem(item2);
 	  
+	  // finalização do pedido
+	  
+	  String notaFiscal = "";
+	  
 	  for(Item item : Pedido.getPedido()) {
-		System.out.println(item.getProduto().getNome() + " x" + item.getQuantidade());
+		  notaFiscal += item.getProduto().getNome() + " x" + item.getQuantidade() + " (" + item.getProduto().getEstoque() + ")\n";
 	  }
 	  
-	  double total = pedido1.calcularTotal();
-	  System.out.printf("Total: R$");
-	  System.out.printf("%.2f\n", total);
+	  System.out.println("Resumo do Pedido:\n" + notaFiscal);
 	  
+	  double total = pedido1.calcularTotal();
+	  
+	  int i = JOptionPane.showConfirmDialog(null, "Total: R$" + total + "\n" + notaFiscal, "Confirmar Pedido", JOptionPane.YES_NO_OPTION);
+	  if(i == JOptionPane.YES_OPTION) {
+		  JOptionPane.showMessageDialog(null, "Pedido Finalizado!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+	  pedido1.setStatus("FINALIZADO");
+	  System.out.println("Estoque Atualizado!");
+	  for(Item item : Pedido.getPedido()) {
+			  item.getProduto().setEstoque(item.getProduto().getEstoque() - item.getQuantidade());
+			  System.out.println(item.getProduto().getNome() + " - Estoque: " + item.getProduto().getEstoque());
+		  }
+		  return;
+	  }
+	  else if(i == JOptionPane.NO_OPTION) {
+		  JOptionPane.showMessageDialog(null, "Pedido Cancelado!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+			  return;
+	  }
   }
 }
